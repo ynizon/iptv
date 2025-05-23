f2217748f3db85115187ae872cfd561ef714e6d5
ynizon <ynizon@gmail.com>
2025-05-23 16:41:16 +0200

    Fix category table

--- database/migrations/0003_01_01_000002_create_categories_table.php
+++ database/migrations/0003_01_01_000002_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('from');
             $table->string('to')->default('')->nullable();
+            $table->timestamps();
         });
     }
 
